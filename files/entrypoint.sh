0d432cf5700b66f05fe1a971cb32dc264242ecb2
Peter Stadler <stadlerpeter@yahoo.fr>
2019-04-26 16:49:34 +0200

    add hack changing getBaseUrl to allow protocol-relative links per default
    credits to https://forum.pkp.sfu.ca/t/ojs-3-behind-reverse-proxy-how-to-achieve/25055/9

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,4 +6,12 @@ source /etc/ojs.config.env
 # Creates OJS configuration file
 config-creator > ${APP_DIR}/config.inc.php
 
+# When running behind a SSL-terminating reverse proxy
+# some hacking is necessary to get SSL to work
+# see https://forum.pkp.sfu.ca/t/ojs-3-behind-reverse-proxy-how-to-achieve/25055/9
+# Simply set $HACK_BASE_URL to some value and it will be fixed here
+if [ -n "$HACK_BASE_URL" ]; then
+    sed -i 's+function getBaseUrl($allowProtocolRelative = false)+function getBaseUrl($allowProtocolRelative = true)+' ${APP_DIR}/lib/pkp/classes/core/PKPRequest.inc.php
+fi
+
 exec "$@"
