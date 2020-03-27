762f917df6da252183df62fba0f7dd259f8875fb
Peter Stadler <stadlerpeter@yahoo.fr>
2020-03-27 10:16:37 +0100

    set php settings to production values

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,6 +6,12 @@ source /etc/ojs.config.env
 # Creates OJS configuration file
 config-creator > ${APP_DIR}/config.inc.php
 
+# set php settings to production values
+# NB this has to be inserted after the previous call
+# otherwise the config-creator script won't be able to
+# access the $_ENV array
+ln -s ${PHP_INI_DIR}/php.ini-production ${PHP_INI_DIR}/php.ini
+
 # When running behind a SSL-terminating reverse proxy
 # some hacking is necessary to get SSL to work
 # see https://forum.pkp.sfu.ca/t/ojs-3-behind-reverse-proxy-how-to-achieve/25055/9
